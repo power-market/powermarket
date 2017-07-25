2afd4082dd6004f89b2a41c44a37e0734069d598
Brian Bohme <bohmebrian@gmail.com>
2017-07-25 16:49:27 -0400

    fixed product spacing

--- db/models/product.js
+++ db/models/product.js
@@ -1,10 +1,6 @@
 'use strict'
 
-<<<<<<< HEAD
 const { STRING, INTEGER } = require('sequelize')
-=======
-const {STRING, INTEGER} = require('sequelize')
->>>>>>> 7929c430ab3dace2f51867f047d74f546d1c2585
 
 module.exports = db => db.define('product', {
   name: {
@@ -30,7 +26,6 @@ module.exports = db => db.define('product', {
     allowNull: false
   }
 }, {
-<<<<<<< HEAD
     getterMethods: {
       price: function () {
         const dollarAmt = this.getDataValue('price') / 100
@@ -46,21 +41,4 @@ module.exports = db => db.define('product', {
 
 module.exports.associations = (Product, { Order, }) => {
   Product.belongsToMany(Order, { as: 'item', through: })
-=======
-  getterMethods: {
-    price: function() {
-      const dollarAmt = this.getDataValue('price') / 100
-      return dollarAmt.toFixed(2)
-    }
-  },
-  setterMethods: {
-    price: function(dollars) {
-      this.setDataValue('price', dollars * 100)
-    }
-  }
-})
-
-module.exports.associations = (Product, {Order, }) => {
-  Product.belongsToMany(Order, {as: 'item', through: })
->>>>>>> 7929c430ab3dace2f51867f047d74f546d1c2585
 }
