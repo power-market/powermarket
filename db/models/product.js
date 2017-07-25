aeff835ba24c90d65279304dbec19921c3233b57
David Yoon <imdavidy@gmail.com>
2017-07-25 16:13:00 -0400

    model association

---
+++ db/models/product.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const {STRING, INTEGER} = require('sequelize')
+
+module.exports = db => db.define('product', {
+  name: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  imageUrl: {
+    type: STRING,
+    defaultValue: '/default.jpg'
+  },
+  description: {
+    type: STRING
+  },
+  price: {
+    type: INTEGER,
+    allowNull: false
+  },
+  count: {
+    type: INTEGER,
+    allowNull: false
+  }
+}, {
+  getterMethods: {
+    price: function() {
+      const dollarAmt = this.getDataValue('price') / 100
+      return dollarAmt.toFixed(2)
+    }
+  },
+  setterMethods: {
+    price: function(dollars) {
+      this.setDataValue('price', dollars * 100)
+    }
+  }
+})
+
+module.exports.associations = (Product, {Order, }) => {
+  Product.belongsToMany(Order, {as: 'item', through: })
+}
