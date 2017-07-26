99f63968d88342f62444972804a171f82e8d90d3
Aatish K Varma <aatish17varma@gmail.com>
2017-07-26 11:41:40 -0400

    put route

---
+++ server/product.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const db = require('APP/db')
+const Product = db.model('product')
+
+// const {mustBeLoggedIn, forbidden} = require('./auth.filters')
+
+module.exports = require('express').Router()
+  .get('/',
+    (req, res, next) =>
+      Product.findAll()
+        .then(product => res.json(product))
+        .catch(next))
+  .post('/',
+    (req, res, next) =>
+      Product.create(req.body)
+      .then(user => res.status(201).json(user))
+      .catch(next))
+  .get('/:productId',
+    (req, res, next) =>
+      Product.findById(req.params.productId)
+      .then(product => res.json(product))
+      .catch(next))
+
+  .put('/:productId', (req, res, next) => {
+      Product.findById(req.params.productId)
+      .then((foundProduct) => {
+        return foundProduct.update(req.body);
+      })
+      .then((updatedProduct) => {
+        res.json(updatedProduct).sendStatus(201)
+      })
+      .catch(next)
+  })
+
