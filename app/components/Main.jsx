5a3a8cf7b5701c53187f28660b9e94799a5fe139
Brian Bohme <bohmebrian@gmail.com>
2017-07-31 15:54:39 -0400

    temp

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,24 +4,11 @@ import { fetchProducts } from "../reducers/product"
 import { connect } from 'react-redux'
 import { NavLink } from "react-router-dom"
 import { withRouter } from 'react-router'
-
-
 import Search from "../reducers/search"
 
-render() {
-    var displayItems = []
-    this.props.search.length > 0 ? displayItems = this.props.search : displayItems = this.props.products
+export class Main extends Component {
 
-    componentWillMount(){
-        this.props.fetchProducts()
-    }
-    compareSearchStoreToProductStore(){
-        if (this.props.search.length)
-            this.setState({ allProducts: this.props.search });
-    }
-
-
-    render(){
+    render() {
         var displayItems = [];
         this.props.search.length > 0 ? displayItems = this.props.search : displayItems = this.props.products;
         console.log(displayItems);
@@ -34,56 +21,33 @@ render() {
                     </div>
                 </div>
                 <div className="row">
-<<<<<<< HEAD
-                {
-                    displayItems.map(eachProduct => {
-                        return (
-                            <div className="col-xs-4" key={eachProduct.id}>
-                                <NavLink className="thumbnail" to={`/products/${eachProduct.id}`}>
-                                    <img src={eachProduct.imageUrl} className="img-thumbnail" style={{ height: '250px', width: '700px' }} />
-                                    <div className="caption">
-                                        <h5>
-                                            <span>{eachProduct.name}</span>
-                                        </h5>
-                                    </div>
-                                </NavLink>
-                            </div>)
-                    })
-=======
-           {
-            displayItems.map(eachProduct => {
-            return (
-            <div className="col-xs-4" key={ eachProduct.id }>
-                <Link className = "thumbnail" to = {`/`}>
-                <img src={ eachProduct.imageUrl } className = "img-thumbnail" style = {{height: '250px', width: '700px'}}/>
-                <div className="caption">
-                  <h5>
-                    <span>{ eachProduct.name }</span>
-                  </h5>
+                    {
+                        displayItems.map(eachProduct => {
+                            return (
+                                <div className="col-xs-4" key={eachProduct.id}>
+                                    <NavLink className="thumbnail" to={`/products/${eachProduct.id}`}>
+                                        <img src={eachProduct.imageUrl} className="img-thumbnail" style={{ height: '250px', width: '700px' }} />
+                                        <div className="caption">
+                                            <h5>
+                                                <span>{eachProduct.name}</span>
+                                            </h5>
+                                        </div>
+                                    </NavLink>
+                                </div>)
+                        })
+                    }
                 </div>
-                </Link>
-            </div>)
-           })
->>>>>>> master
-                }
-            </div>
             </div >
         )
     }
 }
-<<<<<<< HEAD
+
 
 const mapStateToProps = function (state) {
     return {
         products: state.products,
         search: state.search
-=======
-   const mapStateToProps = function(state){
-        return {
-            products: state.products,
-            search: state.search
-        }
->>>>>>> master
     }
+}
 
-    export default withRouter(connect(mapStateToProps)(Main))
+export default withRouter(connect(mapStateToProps)(Main))
