d8e4cf58d97794b3b9e61662309476946c614e07
Brian Bohme <bohmebrian@gmail.com>
2017-08-01 11:04:31 -0400

    test commit

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -3,25 +3,25 @@ import { connect } from 'react-redux'
 import Category from './Category'
 
 class Sidebar extends React.Component {
-    render() {
-        const { products } = this.props
-        const elemental = products.filter(product => product.category === 'elemental')
-        const adaptation = products.filter(product => product.category === 'adaptation')
-        const mental = products.filter(product => product.category === 'mental')
-        return (
-            <sidebar className='col-xs-2'>
-                <div className="sidebar-header">
-                    <h3 href="/">
-                        <div>Categories</div>
-                    </h3>
-                </div>
-                {/* <Category props={elemental} />
+  render() {
+    const { products } = this.props
+    const elemental = products.filter(product => product.category === 'elemental')
+    const adaptation = products.filter(product => product.category === 'adaptation')
+    const mental = products.filter(product => product.category === 'mental')
+    return (
+      <sidebar className='col-xs-2'>
+        <div className="sidebar-header">
+          <h3 href="/">
+            <div>Categories</div>
+          </h3>
+        </div>
+        {/* <Category props={elemental} />
                 <Category props={adaptation} />
                 <Category props={mental} /> */}
 
-            </sidebar>
-        )
-    }
+      </sidebar>
+    )
+  }
 }
 const mapStateToProps = ({ products }) => ({ products })
 
